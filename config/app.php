e0c4c6262c0f6afe54374f2dc55c8e3bdfdd2ef0
Shift <shift@laravelshift.com>
2023-10-17 10:06:17 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Facade;
+
 return [
 
     /*
@@ -207,63 +209,22 @@ return [
     |
     */
 
-    'aliases' => [
-
-        'App' => Illuminate\Support\Facades\App::class,
-        'Arr' => Illuminate\Support\Arr::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'Date' => Illuminate\Support\Facades\Date::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
-        'Event' => Illuminate\Support\Facades\Event::class,
-        'File' => Illuminate\Support\Facades\File::class,
-        'Gate' => Illuminate\Support\Facades\Gate::class,
-        'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Http' => Illuminate\Support\Facades\Http::class,
-        'Js' => Illuminate\Support\Js::class,
-        'Inspiring' => Illuminate\Foundation\Inspiring::class,
-        'Lang' => Illuminate\Support\Facades\Lang::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
-        'Queue' => Illuminate\Support\Facades\Queue::class,
-        'RateLimiter' => Illuminate\Support\Facades\RateLimiter::class,
-        'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-        'Request' => Illuminate\Support\Facades\Request::class,
-        'Response' => Illuminate\Support\Facades\Response::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'Storage' => Illuminate\Support\Facades\Storage::class,
-        'Str' => Illuminate\Support\Str::class,
-        'URL' => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-        //        'Html'      => Illuminate\Html\HtmlFacade::class,
-        //        'Form'      => Illuminate\Html\FormFacade::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+    'aliases' => Facade::defaultAliases()->merge([
+        'AffiliateEarningsByDateFilter' => App\Commands\AffiliateEarningsByDateFilter::class,
         'Carbon' => \Carbon\Carbon::class,
-        'GetAffiliatesCompanyIDPair' => App\Commands\GetAffiliatesCompanyIDPair::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Form' => Collective\Html\FormFacade::class,
         'GetAdvertisersCompanyIDPair' => App\Commands\GetAdvertisersCompanyIDPair::class,
+        'GetAffiliatesCompanyIDPair' => App\Commands\GetAffiliatesCompanyIDPair::class,
         'GetAvailableUsers' => App\Commands\GetAvailableUsers::class,
         'GetCampaignListAndIDsPair' => App\Commands\GetCampaignListAndIDsPair::class,
-        'GetUserActionPermission' => App\Commands\GetUserActionPermission::class,
         'GetInternalAffiliatesCompanyIDPair' => App\Commands\GetInternalAffiliatesCompanyIDPair::class,
+        'GetUserActionPermission' => App\Commands\GetUserActionPermission::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Inspiring' => Illuminate\Foundation\Inspiring::class,
         'RandomProbability' => App\Commands\RandomProbability::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'AffiliateEarningsByDateFilter' => App\Commands\AffiliateEarningsByDateFilter::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class,
         'SSH' => Collective\Remote\RemoteFacade::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
-    ],
+    ])->toArray(),
 ];
